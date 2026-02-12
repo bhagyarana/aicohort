e00180db5be54c40be7d570b1640258f6c73ee0d
Bhagya Vinod Rana <bhagyarana2001@gmail.com>
2026-02-12 11:51:32 +0530

    minor cta fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,7 +65,7 @@ function HeroSection() {
             <Link
               className={styles.heroCTA}
               to="/learn/onboarding">
-              Explore Curriculum
+              Start Learning
               <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <line x1="5" y1="12" x2="19" y2="12" />
                 <polyline points="12 5 19 12 12 19" />
